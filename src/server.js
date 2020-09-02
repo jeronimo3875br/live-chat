e98ebbbb524a95caf5b826659dc09259105bb710
= <=>
2020-09-02 18:47:32 -0300

    update file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const app = require("../src/app");
+const app = require("./app");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const debug = require("debug")("nodestr:server");
